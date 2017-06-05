e1e991b5ac410f247b1d2b20411fce2c3f1752cb
coviam <vsviveksingh30@gmail.com>
2017-06-06 02:40:15 +0530

    Added microservice ip into application.properties files

--- src/main/java/com/coviam/doodle/doodle/controller/RestApiController.java
+++ src/main/java/com/coviam/doodle/doodle/controller/RestApiController.java
@@ -1,19 +1,29 @@
 package com.coviam.doodle.doodle.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.coviam.doodle.doodle.PathProperties;
+
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
 	
-	 @RequestMapping(value = "/listProd/{key}", method = RequestMethod.GET)
+		private PathProperties paths;
+		@Autowired
+	    public void setProduct(PathProperties paths) {
+	        this.paths = paths;
+	    }
+		@RequestMapping(value = "/listProd/{key}", method = RequestMethod.GET)
 	    public Object listProductsByCategory(@PathVariable("key") String key) {
 	       
-		 final String uri = "http://172.16.20.10:8080/getProductByCategory/1";
+
+		 final String uri = paths.getProduct() +"/getProductByCategory/1";
+		 
 		 RestTemplate restTemplate = new RestTemplate();
 		 String result = restTemplate.getForObject(uri, String.class);
 		 return result;
