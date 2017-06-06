6940c29741e3a26e0e109fd5926dd4b1a5d00fca
nihalgurjar007 <coviam@Coviams-MacBook-Pro-2.local>
2017-06-06 15:23:51 +0530

    productlist.html updated

--- src/main/resources/static/js/app/ListController.js
+++ src/main/resources/static/js/app/ListController.js
@@ -22,6 +22,9 @@
               "seller":5 
             }];
             console.log("In ListController");
+            $scope.category = ["Laptop", "Mobile", "TV","SmartPhones","Power bank","USB","Chargers"];
+          });
+            
             var jsonData=myService.getData('http://localhost:8080/api/listProd/1');
             jsonData.then(function(result){
                 $scope.content =result;
